66eadb835c861eee3ba8cb1e5762be24bd95bf18
Gate <luchangxiang1@163.com>
2015-10-27 21:13:34 +0800

    Initial Commit

---
+++ Get&Post/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Get&Post
+//
+//  Created by Gate on 15/10/27.
+//  Copyright © 2015年 Gate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Get&PostTests/Get_PostTests.m
@@ -0,0 +1,39 @@
+//
+//  Get_PostTests.m
+//  Get&PostTests
+//
+//  Created by Gate on 15/10/27.
+//  Copyright © 2015年 Gate. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Get_PostTests : XCTestCase
+
+@end
+
+@implementation Get_PostTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
